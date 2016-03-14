99d8c66f22e8b8a17b24dd4de0f42bd9e032ff6f
sylxjtu <a443833359@gmail.com>
2016-03-14 22:02:49 +0800

    uva101fix

--- uva/uva101.cpp
+++ uva/uva101.cpp
@@ -1,101 +1,130 @@
-#include <cstdio>
 #include <iostream>
 #include <vector>
-#include <cassert>
 using namespace std;
 
-vector<int> blocks[25];
-int n;
+struct BlockPile{
+    vector<int> data;
+    void print(){
+        for (int i = 0; i < data.size(); ++i)
+        {
+            cout << " " << data[i];
+        }
+    }
+    int find(int target, int& retOffset){
+        for (int i = 0; i < data.size(); ++i)
+        {
+            if (data[i] == target)
+            {
+                retOffset = i;
+                return 1;
+            }
+        }
+        return 0;
+    }
+};
 
-vector<int>* posof(int x, int& h){
-    for(int i=0; i<n; i++){
-        for(int j=0; j<blocks[i].size(); j++){
-            if(blocks[i][j]==x){
-                h=j;
-                return blocks+i;
+struct BlockWorld{
+    BlockPile* s;
+    int size;
+    BlockWorld(int sz){
+        size=sz;
+        s = new BlockPile[sz];
+        for (int i = 0; i < sz; ++i)
+        {
+            s[i].data.push_back(i);
+        }
+    }
+    ~BlockWorld(){
+        delete []s;
+    }
+    void print(){
+        for (int i = 0; i < size; ++i)
+        {
+            cout << i << ":";
+            s[i].print();
+            cout << endl;
+        }
+    }
+    void find(int target, int& retPile, int& retOffset){
+        for (int i = 0; i < size; ++i)
+        {
+            if (s[i].find(target, retOffset))
+            {
+                retPile = i;
+                return;
             }
         }
     }
-    assert(0);
-}
-
-vector<int>* pileof(int x){
-    int t;
-    return posof(x,t);
-}
-
-void pileover(int op1, int op2){
-    int h1,h2;
-    vector<int>* p1=posof(op1,h1), * p2=posof(op2,h2);
-    for (int i = h1; i < p1->size(); i++) {
-        p2->push_back((*p1)[i]);
-    }
-    int p1s=p1->size();
-    for (int i = h1; i < p1s; i++) {
-        p1->pop_back();
-    }
-}
-
-void replace(int x){
-    vector<int>* p = pileof(x);
-    while(p->back() != x) {
-        blocks[p->back()].push_back(p->back());
-        p->pop_back();
-    }
-    blocks[p->back()].push_back(p->back());
-    p->pop_back();
-}
-
-void print(){
-    for (int i = 0; i < n; i++) {
-        cout << i << ": ";
-        for (int j = 0; j < blocks[i].size(); j++) {
-            if (j!=0) {
-                cout << " ";
-            }
-            cout << blocks[i][j];
+    void clearPos(int pile, int offset){
+        for (int i = offset+1; i < s[pile].data.size(); ++i)
+        {
+            s[s[pile].data[i]].data.push_back(s[pile].data[i]);
         }
-        cout << endl;
+        s[pile].data.resize(offset+1);
     }
-}
+    void clearValue(int target){
+        int pile, offset;
+        find(target, pile, offset);
+        clearPos(pile, offset);
+    }
+    void moveTo(int src, int dest){
+        int srcPile, destPile, devNull;
+        find(src, srcPile, devNull);
+        find(dest, destPile, devNull);
+        s[destPile].data.push_back(s[srcPile].data.back());
+        s[srcPile].data.pop_back();
+    }
+    void pileTo(int src, int dest){
+        int srcPile, destPile, srcOffset, destOffset;
+        find(src, srcPile, srcOffset);
+        find(dest, destPile, destOffset);
+        for (int i = srcOffset; i < s[srcPile].data.size(); ++i)
+        {
+            s[destPile].data.push_back(s[srcPile].data[i]);
+        }
+        s[srcPile].data.resize(srcOffset);
+    }
+};
 
 int main(){
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        blocks[i].push_back(i);
-    }
+    int sz;
+    cin >> sz;
+    BlockWorld bw(sz);
     while(1){
-        string s1,s2;
-        int op1,op2;
+        string s1;
         cin >> s1;
-        if (s1=="quit") {
-            print();
+        if (s1 == "quit")
+        {
+            bw.print();
             return 0;
         }
-        cin >> op1 >> s2 >> op2;
-        if (pileof(op1)==pileof(op2)){
-            continue;
+        else
+        {
+            int src, dest;
+            string s2;
+            cin >> src >> s2 >> dest;
+
+            int srcPile, destPile, devNull;
+            bw.find(src, srcPile, devNull);
+            bw.find(dest, destPile, devNull);
+            if (srcPile == destPile)
+            {
+                continue;
+            }
+
+            if (s2 == "onto")
+            {
+                bw.clearValue(dest);
+            }
+            if (s1 == "move")
+            {
+                bw.clearValue(src);
+                bw.moveTo(src, dest);
+            }
+            else
+            {
+                bw.pileTo(src, dest);
+            }
         }
-        if (s1=="move" && s2=="onto"){
-            replace(op1);
-            replace(op2);
-            pileof(op1)->pop_back();
-            pileof(op2)->push_back(op1);
-        }
-        if (s1=="move" && s2=="over"){
-            replace(op1);
-            pileof(op1)->pop_back();
-            pileof(op2)->push_back(op1);
-        }
-        if (s1=="pile" && s2=="onto"){
-            replace(op2);
-            pileover(op1,op2);
-        }
-        if (s1=="pile" && s2=="over"){
-            pileover(op1,op2);
-        }
-#ifdef DEBUG
-        print();
-#endif
     }
 }
